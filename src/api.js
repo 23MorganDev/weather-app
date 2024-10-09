1b1aa37d5c2e4f763f0d2bf9b03d6fa3e715a033
Morgan Muthee <125727893+23MorganDev@users.noreply.github.com>
2024-10-09 16:47:47 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions= {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': import.meta.env.VITE_RAPIDAPI_KEY,,
+        'X-RapidAPI-Key': import.meta.env.VITE_RAPIDAPI_KEY,
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
